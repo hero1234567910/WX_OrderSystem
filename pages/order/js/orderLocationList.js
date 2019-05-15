7247fb3c4996581cdfd021e4c673a4a93fc3bb7b
wzlggg <wzl1888@126.com>
2019-05-15 17:18:07 +0800

    地址跳转

--- pages/order/js/orderLocationList.js
+++ pages/order/js/orderLocationList.js
@@ -50,7 +50,7 @@ $('.weui-cell.weui-cell_access').click(function(){
 	var hosInpatient = $(this).children('.weui-cell__bd').children('.loca-x').children('.p-location').children('#inpatient').text().trim();
 	var hosStorey = $(this).children('.weui-cell__bd').children('.loca-x').children('.p-location').children('#storey').text().trim();
 	var hosBedNumber = $(this).children('.weui-cell__bd').children('.loca-x').children('.p-location').children('#bedNumber').text().trim();
-	location.href = "../order/orderSubmit.html?hosUserName=" + hosUserName + "&hosUserMobile=" + hosUserMobile +
+	location.href = "../order/orderLocation.html?hosUserName=" + hosUserName + "&hosUserMobile=" + hosUserMobile +
 		"&hosInpatient=" + hosInpatient +
 		"&hosStorey=" + hosStorey + "&hosBedNumber=" + hosBedNumber;
 })
